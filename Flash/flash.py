768c27516479ac7df4f7d8851b0773bdb14d77db
nikhilwbhatia <>
2018-11-18 18:42:47 -0500

    fixed flashing

--- Flash/flash.py
+++ Flash/flash.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5 import QtGui, QtCore, QtWidgets, QtTest
 
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout, QGridLayout
 from PyQt5.QtGui import QIcon
@@ -21,23 +21,34 @@ class Dialog(QtWidgets.QWidget):
 
         self.setLayout(layout)
 
-    def flashSplash(self):
+    def flashSplash(self, path):
         # Be sure to keep a reference to the SplashScreen
         # otherwise it'll be garbage collected
         # That's why there is 'self.' in front of the name
-        self.splash = QtWidgets.QSplashScreen(QtGui.QPixmap('./ex.jpg'))
+        self.splash = QtWidgets.QSplashScreen(QtGui.QPixmap(path))
 
         # SplashScreen will be in the center of the screen by default.
         # You can move it to a certain place if you want.
         # self.splash.move(10,10)
         self.splash.show()
 
+
         # Close the image after 100ms
         QtCore.QTimer.singleShot(100, self.splash.close)
 
+
+
     def keyPressEvent(self, e):
         if e.key() == QtCore.Qt.Key_Space:
-            self.flashSplash()
+            for i in range(5):
+                self.flashSplash('./ex.jpg')
+                QtTest.QTest.qWait(3000)
+
+
+    #
+    # def keyPressEvent2(self, e):
+    #     if e.key() == QtCore.Qt.Key_Escape:
+    #         self.flashSplash()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
